9329a50eddaecd8ff48b8bf6e24ccf711df28c41
DevdutMS <dmsdevdut@gmail.com>
2022-06-10 14:57:55 +0530

    Features updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      // xxxs: {'max': '320px'},
+      // xxs: {'max': '360px'}, // min-width
+      // xs: {'max': '420px'}, // min-width
+      // "sm.5": {'max': '541px'},
+      xxxs: "320px",
+      xxs: "360px",
+      xs: "420px",
+      "sm.5": "541px",
+      "2md": "769px",      
+ 
+      ...defaultTheme.screens,
+
+    },
+
     extend: {
       screens: {
-        xxxs: "320px",
-        xxs: "360px", // min-width
-        xs: "420px", // min-width
-        "sm.5": "541px",
         "3xl": "1540px",
-        "2md": "769px",
       },
       colors: {
         "primary-black": "#242A39",
@@ -18,11 +29,15 @@ module.exports = {
         "primary-light-yellow": "#FFEE9B",
         "primary-light-gray": "#FEFEFE",
         "primary-blue-03": "#6478D3",
-        "primary-light-gray-01" : "#F5F7FA",
-        "primary-blue-04" : "#F2F2FF"
+        "primary-light-gray-01": "#F5F7FA",
+        "primary-blue-04": "#F2F2FF",
+        "primary-blue-05": "#FBFBFF",
+        "primary-light-gray-02": "#FEFEFF",
+        "primary-violet": "#9E8BFF",
       },
       fontSize: {
         xxs: "11.5px",
+        "xs.5": "13px",
         "sm.5": "15px",
         "3.5xl": "31px",
         "4.5xl": "42px",
@@ -35,10 +50,16 @@ module.exports = {
       boxShadow: {
         "4xl": "0 2px 5px  rgb(41 32 91 / 8%), 0 3px 4px  rgb(36 42 57 / 4%)",
         "5xl": "1px 5px  rgb(41 32 91 / 8%), 0 3px 4px  rgb(36 42 57 / 8%)",
+        violet: "0 4px 8px rgb(158 139 255 / 45%)",
       },
       borderRadius: {
-        "4xl":"32px"
-      }
+        "4xl": "32px",
+      },
+      lineHeight: {
+        20: "20px",
+        12: "12px",
+        15: "15px",
+      },
     },
   },
   plugins: [],
